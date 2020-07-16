9682793656fcea1253dfaa0ff2c3d5cb1265c20b
Nikola Mitic <nikola.mitic1995@gmail.com>
2020-07-16 05:07:39 +0200

    Updates

---
+++ precache-manifest.fc95d023e03c38c8369277998f423d3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "decd4f4b28af5e25b3c7dc312df8f06a",
+    "url": "/Bit-People/index.html"
+  },
+  {
+    "revision": "6e2bd7ee1424294d181e",
+    "url": "/Bit-People/static/css/2.6cc87aa2.chunk.css"
+  },
+  {
+    "revision": "032aa231cdc756bfafa9",
+    "url": "/Bit-People/static/css/main.1030fb37.chunk.css"
+  },
+  {
+    "revision": "6e2bd7ee1424294d181e",
+    "url": "/Bit-People/static/js/2.41a8d376.chunk.js"
+  },
+  {
+    "revision": "35937c55f4ccbb69aaefc08b51c916a0",
+    "url": "/Bit-People/static/js/2.41a8d376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032aa231cdc756bfafa9",
+    "url": "/Bit-People/static/js/main.baab454d.chunk.js"
+  },
+  {
+    "revision": "8528ed57a073a490ffaf",
+    "url": "/Bit-People/static/js/runtime-main.b851c0a3.js"
+  },
+  {
+    "revision": "74262339fa4cc98af5157734e0637719",
+    "url": "/Bit-People/static/media/not_found.74262339.png"
+  }
+]);
